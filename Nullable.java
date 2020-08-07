fd748f2faf1e87e81675a6c52cdfc872e96dfcc1
Gnxela <qawswedfrgtt@gmail.com>
2020-08-07 12:36:49 +0100

    Moved project to Maven

--- Nullable.java
+++
@@ -1,5 +0,0 @@
-package me.alexng.untitled;
-
-public @interface Nullable {
-	// TODO: Use library
-}
---
+++ src/main/java/me/alexng/untitled/Nullable.java
@@ -0,0 +1,5 @@
+package me.alexng.untitled;
+
+public @interface Nullable {
+	// TODO: Use library
+}
