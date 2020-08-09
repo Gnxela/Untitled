df3e4662b0f2b34c7339dda2202b4dde40d33b16
Gnxela <qawswedfrgtt@gmail.com>
2020-08-09 14:37:58 +0100

    Added element buffer object.

--- src/main/java/me/alexng/untitled/Main.java
+++ src/main/java/me/alexng/untitled/Main.java
@@ -19,9 +19,14 @@ public class Main {
 	public static void main(String[] args) throws IOException, UntitledException {
 		Window window = Window.create(WIDTH, HEIGHT, TITLE);
 		float[] vertexData = {
-				-0.5f, -0.5f, 0.0f,
-				0.5f, -0.5f, 0.0f,
-				0.0f, 0.5f, 0.0f,
+				0.5f, 0.5f, 0.0f, // top right
+				0.5f, -0.5f, 0.0f, // bottom right
+				-0.5f, -0.5f, 0.0f, // bottom left
+				-0.5f, 0.5f, 0.0f // top left
+		};
+		int[] indexData = {
+				0, 1, 3, // first triangle
+				1, 2, 3 // second triangle
 		};
 
 		ShaderProgram shaderProgram = new ShaderProgram();
@@ -36,6 +41,10 @@ public class Main {
 		glBindBuffer(GL_ARRAY_BUFFER, vbo);
 		glBufferData(GL_ARRAY_BUFFER, vertexData, GL_STATIC_DRAW);
 
+		int ebo = glGenBuffers();
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexData, GL_STATIC_DRAW);
+
 		glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * 4, 0);
 		glEnableVertexAttribArray(0);
 
@@ -43,7 +52,7 @@ public class Main {
 			window.clear();
 			glBindVertexArray(vao);
 			glUseProgram(shaderProgram.getHandle());
-			glDrawArrays(GL_TRIANGLES, 0, 3);
+			glDrawElements(GL_TRIANGLES, indexData.length, GL_UNSIGNED_INT, 0);
 			window.update();
 		}
 		shaderProgram.cleanup();
